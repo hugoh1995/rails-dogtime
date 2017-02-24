01138acd453e230492034ffd2c6f95deadd0d541
hugo houyez <hugohconcordia@gmail.com>
2017-02-24 16:52:18 +0100

    remove breed obligation

--- app/models/quantity.rb
+++ app/models/quantity.rb
@@ -2,6 +2,6 @@ class Quantity < ApplicationRecord
   belongs_to :request
   belongs_to :breed
 
-  validates :breed, uniqueness: { scope: :request }
+  # validates :breed, uniqueness: { scope: :request }
 
 end
